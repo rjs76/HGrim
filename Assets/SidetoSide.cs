a4ab4de2fda4cdfc87da1290cd6dcdbf53e77b8a
Riviere Seguie <rjs76@njit.edu>
2018-11-02 21:00:56 -0400

    Sidetoside Platform

---
+++ Assets/SidetoSide.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidetoSide : MonoBehaviour {
+    float dirX, moveSpeed = 2f;
+    bool moveright = true; 
+	// Use this for initialization
+	void Update () {
+        if (transform.position.x > 57f)
+            moveright = false;
+        if (transform.position.x < 55f)
+            moveright = true;
+
+        if (moveright)
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+    }
+
+}
