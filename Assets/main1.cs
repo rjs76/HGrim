80daf3a736cc548f5985dc3315f27477f996a49b
Riviere <rjs76@njit.edugit config --global user.name Rivieregit config --global user.email rjs76@njit.edu>
2018-12-21 14:57:59 -0500

    Spanish Translator

---
+++ Assets/main1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main1 : MonoBehaviour {
+
+    public void Spanish()
+         {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+         }
+    public void Playgame()
+         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+         }
+   
+
+
+        public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
+
---
+++ Assets/main2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main2 : MonoBehaviour {
+
+
+    public void English()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+    public void Playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
